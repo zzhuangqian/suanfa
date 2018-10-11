3c16b29d75b8cae0fcf71343e5568db315ce15c0
zzhuangqian <18701626636@163.com>
2018-10-11 18:09:15 +0800

    图

---
+++ Graph/DenseGraph.js
@@ -0,0 +1,14 @@
+class DenseGraph{
+  constructor(n,directed= false){
+    // 节点数
+    this.n = n
+    // 边数
+    this.m = 0
+    // 是否是有向的
+    this.directed = directed
+    for(let i =0;i< n; i++){
+      this.g.push_back(new Ver)
+    }
+    
+  }
+}
